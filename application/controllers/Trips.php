697748bd5cd14251af33c9a1ad41018f18860482
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-04-25 11:06:39 -0700

    updated controller file

--- application/controllers/Trips.php
+++ application/controllers/Trips.php
@@ -41,7 +41,7 @@ class Trips extends CI_Controller{
 		$this->form_validation->set_rules("password", "Password", "trim|required|min_length[8]|do_hash");
 
 		if($this->form_validation->run() == FALSE){
-			$errors = $this->MY_form_validation->getErrorsArray();
+			$errors = $this->form_validation->getErrorsArray();
 			var_dump($errors);
 			die();
 			// $this->session->set_flashdata("errors2", $errors);
