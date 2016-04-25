b7a83324afb5719d42c1190162ff315a5f63798c
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-04-25 10:24:39 -0700

    updated controller file

--- application/controllers/Trips.php
+++ application/controllers/Trips.php
@@ -18,7 +18,7 @@ class Trips extends CI_Controller{
 		$this->form_validation->set_rules("name", "Full Name", "trim|required|min_length[3]|xss_clean");
 		$this->form_validation->set_rules("username", "Username", "trim|required|min_length[3]|xss_clean");
 		if($this->form_validation->run() === FALSE){
-			$errors = $this->form_validation->getErrorsArray();
+			$errors = validation_errors();
 			$this->session->set_flashdata("errors", $errors);
 			redirect('/');
 		}
@@ -45,7 +45,7 @@ class Trips extends CI_Controller{
 		$this->form_validation->set_rules("password", "Password", "trim|required|min_length[8]|do_hash");
 
 		if($this->form_validation->run() == FALSE){
-			$errors = $this->form_validation->getErrorsArray();
+			$errors = validation_errors();
 			var_dump("errors");
 			die();
 			// $this->session->set_flashdata("errors2", $errors);
